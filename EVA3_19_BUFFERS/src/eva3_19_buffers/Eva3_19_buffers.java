629f9b80f22cad9d208b9830006be5c10d0e9fb2
DajahiraNarahiGonzalezZavala <101366240+DajahiraNarahiGonzalezZavala@users.noreply.github.com>
2022-06-11 15:10:35 -0600

    Add files via upload

---
+++ EVA3_19_BUFFERS/src/eva3_19_buffers/Eva3_19_buffers.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_19_buffers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ *
+ * @author 
+ */
+public class Eva3_19_buffers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //definir la ruta       //paths te ayuda a definir rutas en diferentes formatos,permite relaizar muchas funciones
+        Path ruta = Paths.get("c:/archivo/texton.txt");
+        //abrimos el archivo
+        InputStream abrirArch;
+        //leemos el archivo
+        InputStreamReader leerArch;
+        //convertimos bytes a caracteres
+        BufferedReader leerTexto;
+        
+        try{
+        abrirArch = Files.newInputStream(ruta);
+        leerArch  = new InputStreamReader(abrirArch);
+        String linea = leerTexto.readLine();
+        while(linea !=null){
+            System.out.println(linea);
+            linea = leerTexto.readLine();
+        }
+        leerTexto.close();//siempre es muy importante cerrar el stream
+        }catch(IOException ex){
+    
+}
+  }
+  }
+
+
---
+++ EVA3_20_ESCRIBIR/src/eva3_20_escribir/Eva3_20_ESCRIBIR.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_20_escribir;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ *
+ * @author Narahi
+ */
+public class Eva3_20_ESCRIBIR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Path ruta = Paths.get("c:/Archivo/texto1.txt") 
+                OutputStream abrirArchivo;
+                OUtputStreamWriter escribirArh;
+                BufferedWriter excribirtexto;
+                try{
+                abrirArchivo = Files.newOutputStream(ruta);
+                escribirArh = new OutputStreamWriter(abrirArchivo);
+                
+                excribirtexto = new BufferedWriter(escribirArh);
+                excribirtexto.write("Hola mundo");
+                excribirtexto.flush();
+                excribirtexto.fush();
+                excribirtexto.close();
+                
+                
+                        
+                
+    }  
+                catch(IOException ex){
+                    ex.printStacktTrace;
+                }
+    }
+                
+    
+}
